9cc6441b97210402c3f0944e710bfc7d247efc52
Hassan Tarek <hassantarekfathy@gmail.com>
2026-07-01 04:17:13 +0300

    fix nextauth prisma init issue

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,21 @@
-import { authOptions } from "@/utils/auth";
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const handler = NextAuth(authOptions);
+const prisma = new PrismaClient(); // مهم هنا local فقط
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+});
 
 export { handler as GET, handler as POST };
