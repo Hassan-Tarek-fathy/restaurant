b986637e9063f319032324158b5f84146167ca78
Hassan Tarek <hassantarekfathy@gmail.com>
2026-07-01 04:33:03 +0300

    Fix Prisma build

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient(); // مهم هنا local فقط
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { prisma } from "@/utils/connect";// مهم هنا local فقط
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
