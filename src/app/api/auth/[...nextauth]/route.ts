19aac9c8d3c883015f6caef7022f604bb89cd800
Hassan Tarek <hassantarekfathy@gmail.com>
2026-07-01 15:27:04 +0300

    debug env

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,8 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/utils/connect";// مهم هنا local فقط
-
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET ? "FOUND" : "NOT FOUND");
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
